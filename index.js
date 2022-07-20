6f6674b283dafd17e632ff5894a5c326b8a64e59
ngonpaul <pmgonzalez05@outlook.com>
2022-07-20 15:39:15 -0700

    Corrected port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import cors from "cors";
 const URL = "https://api.kanye.rest/";
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port:${PORT}`);
 });
