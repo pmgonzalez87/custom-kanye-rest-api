08afc1a22ab1f6ec282f6510c0884a557747ecc0
ngonpaul <pmgonzalez05@outlook.com>
2022-07-20 15:02:14 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import express from "express";
+import cors from "cors";
+
+const URL = "https://api.kanye.rest/";
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port:${PORT}`);
+});
+
+app.use(cors());
+//app.use()
+
+app.get(
+  "/kanye_quotes",
+  (rec, res, next) => {
+    console.log("Paul");
+    next();
+  },
+
+  async (req, res) => {
+    try {
+      const response = await axios.get(URL);
+      res.send(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
